f71f9a002ace23f8457d19cf4929308309833068
omayraortiz <83971482+omayraortiz@users.noreply.github.com>
2021-05-10 11:48:16 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.Square)
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
